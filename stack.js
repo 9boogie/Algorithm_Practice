fb98b61096e90f2b2a88f64bd686138c63eaa837
9boogie <jzero.youngs89@gmail.com>
2020-12-05 23:19:48 -0800

    Stack and Queue

---
+++ stack.js
@@ -0,0 +1,14 @@
+let testArray = [];
+
+testArray.push(7);
+testArray.push(4);
+testArray.push(6);
+console.log(testArray);
+testArray.pop();
+testArray.push(1);
+testArray.push(2);
+testArray.pop();
+console.log(testArray);
+
+// Stack is the data stacking and the latest comes out at first
+// Queue is First in, first out 
